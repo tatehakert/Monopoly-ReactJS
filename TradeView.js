9b4a9e8b9d85d8bd00cc4060a4fd0689168d8fdb
Jack Storey <jstorey@smu.edu>
2021-04-21 09:20:14 -0500

    Add files via upload

---
+++ TradeView.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Modal, Button} from 'react-bootstrap';
+import PropertyUpgrades from './ProperyUpgrades';
+
+class TradeModal extends React.Component {
+    //pass in list of all players
+    //DROP DOWN MENU   
+    
+    //<select className="mx-2" value={this.state.numPlayers} onChange={(event) => this.updateNumPlayers(event.target.value)}>
+    //<option value={2} key={"numPlayers-2"}>2</option>
+    //<option value={3} key={"numPlayers-3"}>3</option>
+    //<option value={4} key={"numPlayers-4"}>4</option>
+    //</select>
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+  
+        }
+    }
+
+
+    render() {
+      return (
+        <div> be happy </div>
+        )
+    }
+
+
+    componentDidMount(){
+
+    }
+  }
+  
+  export default TradeModal;
